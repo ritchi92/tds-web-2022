507cb66b917eb7a4c9e48c3a6f93bbd568d9af91
Loïc Théaudin <ltheaudin@sio.local>
2022-12-15 14:41:53 +0100

    update

---
+++ portfolio/index.php
@@ -0,0 +1,169 @@
+<?php
+//include "php/jsp.php";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>loïc Théaudin</title>
+		<link rel="stylesheet" href="css/site.css">
+		<link rel="icon" href="image/favoricon.ico">
+	</head>
+	<body>
+		<header>
+			<div id="bouton">
+				<a href="#top"><img src="image/to_top.png" alt="TOP" /></a>
+			</div>
+				<ul class='menu'>
+					<li><a href='#acceuil'>|Acceuil|</a></li>
+					<li><a href='#propos'>|A propos|</a></li>
+					<li><a href='#competence'>|Compétences|</a></li>
+					<li><a href='#experience'>|Expérience|</a></li>
+					<li><a href='#formation'>|Formation|</a></li>
+					<li><a href='#contact'>|Contact|</a></li>
+				</ul>
+		</header>
+		<main>
+	<div class='backgroundacceuil'>
+		<h1 class='titre'>
+			<?php
+			$data=yaml_parse_file('yaml/aceuille.yaml');
+			//print_r($data);
+			echo $data["titre"];
+			?>
+		</h1>
+			<span id='acceuil'>
+
+				<?php
+					echo '<p>'.$data["nomprenom"].'</p>';
+					echo '<p>'.$data["accroche"].'</p>';
+				?>
+			</span>
+	</div>
+	
+	
+	<div class='backgroundpropos'>
+		<h1 class='titre'>
+			propos
+		</h1>
+		<?php
+		$data=yaml_parse_file('yaml/propos.yaml');
+		?>				
+		<span id='propos'>
+			<?php
+					echo '<p>'.$data["accroche"].'</p>';
+					echo '<p>'.$data["descrip"].'</p>';
+			?>
+		</span>
+	</div>
+
+
+
+
+		
+		<h1 class='titre'>
+			<?php
+			$data=yaml_parse_file('yaml/compétence.yaml');
+			?>
+		</h1>
+	
+		<span id='competence'>
+			<h1 class='titre'>
+				Compétences
+			</h1>
+
+			<div class="container">
+				<?php
+				foreach($data AS $domaine=>$tab){
+					echo '<h1 class="domaine">'.$domaine.'</h1>';
+					foreach($tab AS $nom=>$niveau){
+						echo '<p>'.$nom.'</p>';
+						echo '<div class="progressbar-wrapper">';
+						echo '<div title="css" class="progressbar" style="width:'.$niveau.'%;">'.$niveau.'%</div>';
+						echo '</div>';
+					}
+				}
+				?>	 
+			</div>
+			
+			</span>
+				<?php
+				$data=yaml_parse_file('yaml/experience.yaml');
+				?>
+			<h1 class='titre'>
+				experience
+			</h1>
+			<span id='experience'>
+				<?php
+					echo '<p>'.$data["titre"].'</p>';
+					echo '<p>'.$data["descrip"].'</p>';
+
+				?>
+			</span>
+			<?php
+			$data=yaml_parse_file('yaml/formation.yaml');
+			?>
+			<h1 class='titre'>
+				formation
+			</h1>
+			<span id='formation'>
+				<?php
+					echo '<p>'.$data["titre"].'</p>';
+					echo '<p>'.$data["descrip"].'</p>';
+
+				?>
+			</span>
+			<div class='backgroundcontact'>
+				<?php
+				$data=yaml_parse_file('yaml/contact.yaml');
+				?>
+				<h1 class='titre'>
+					contact
+				</h1>
+			
+				<span id='contact'>
+					<?php
+						echo '<p>'.$data["descrip"].'</p>';
+						echo '<p>'.$data["numero"].'</p>';
+						echo '<p>'.$data["reseaux"].'</p>';
+
+						echo'</span>';
+						echo'<h1 class="titre">'.$data["titre"].'</h1>';
+						echo'<span id="contacter">';
+						
+						echo '<p>'.$data["expediteur"].'</p>';
+						echo '<input type="text" name="prenom" value="" placeholder="votre nom et prénom"/>';
+						
+						echo '<p>'.$data["mdp"].'</p>';
+						echo '<input type="password" name="motdepasse" value="" placeholder="votre mot de passe" />';
+						
+						echo '<p>'.$data["civilite"].'</p>';
+						echo '<input type="radio" name="civi2" value="Mme" /> Madame
+							<input type="radio" name="civi2" value="Mlle" /> Mademoiselle
+							<input type="radio" name="civi2" value="Mr" checked="checked" /> Monsieur';
+						
+						echo '<p>'.$data["adresse"].'</p>';
+						echo'<input type="text" name="adresse" value="" placeholder="votre adresse" />';
+						
+						echo '<p>'.$data["objet"].'</p>';
+						echo '<textarea name="le-message" rows="1" cols="40" placeholder="saisisez votre raison"></textarea>';
+						
+						echo '<p>'.$data["contenu"].'</p>';
+						echo '<textarea name="le-message" rows="6" cols="40" placeholder="saisisez votre message"></textarea>';
+						
+						echo '<p>'.$data["captcha"].'</p>';
+						echo '<input type="radio" name="civi" value="Mme" /> oui
+								<input type="radio" name="civi" value="Mlle" /> non';
+						echo '<p>'.$data["envoi"].'</p>';
+						
+						echo '<input type="submit" value="Envoyer" />';
+						echo ' <input type="reset" value="Annuler" />';
+						
+						mail('loic.theaudin@sts-sio-caen.info','objet','contenu');
+							
+					?>	
+				</span>
+			</div>
+		</main>		
+	</body>
+</html>

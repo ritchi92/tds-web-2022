cb9d8c86668de934cb46c3318718bb2f30a71544
loic <loic.theaudin@gmail.com>
2022-12-20 16:05:21 +0100

    update

--- portfolio/index.php
+++ portfolio/index.php
@@ -149,7 +149,7 @@
 							</form>
 							<?php
 							if (isset($_POST['message'])) {
-								$retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
+								$retour = mail('loic.theaudin@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
 								if($retour)
 									echo '<p>Votre message a bien été envoyé.</p>';
 							}
